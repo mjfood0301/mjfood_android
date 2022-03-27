c23cbee11d9933689c7c9db81bd1ef97cec7789c
lee989898 <wnsrud1352@gmail.com>
2022-03-27 19:07:56 +0900

    feat: 로그인 Intent 추가

--- app/src/main/java/com/lee989898/todayeat/src/login/LoginActivity.kt
+++ app/src/main/java/com/lee989898/todayeat/src/login/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.lee989898.todayeat.src.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lee989898.todayeat.MainActivity
 import com.lee989898.todayeat.R
+import com.lee989898.todayeat.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginNoLoginIv.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.loginKakaoLoginIv.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+
+        }
 
 
     }
