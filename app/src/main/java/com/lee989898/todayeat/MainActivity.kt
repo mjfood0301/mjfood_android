f17b069d97c58972636592ed93b26178f3a4936c
lee989898 <wnsrud1352@gmail.com>
2022-03-30 16:00:56 +0900

    feat: 메인메뉴 엑티비티 이동 기능 추가

--- app/src/main/java/com/lee989898/todayeat/MainActivity.kt
+++ app/src/main/java/com/lee989898/todayeat/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.lee989898.todayeat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lee989898.todayeat.databinding.ActivityMainBinding
+import com.lee989898.todayeat.src.mylist.MyListActivity
+import com.lee989898.todayeat.src.profile.ProfileActivity
+import com.lee989898.todayeat.src.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainMenuSerachIv.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.mainMenuProfileIv.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.mainMenuHeartIv.setOnClickListener {
+            val intent = Intent(this, MyListActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
