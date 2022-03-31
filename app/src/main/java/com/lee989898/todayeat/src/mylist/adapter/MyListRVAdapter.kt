04414d3e6ec524ae8a23e3f34d483bd35083a319
lee989898 <wnsrud1352@gmail.com>
2022-03-31 10:49:53 +0900

    feat: 내 찜 목록 리사이클러 뷰 수정

--- app/src/main/java/com/lee989898/todayeat/src/mylist/adapter/MyListRVAdapter.kt
+++ app/src/main/java/com/lee989898/todayeat/src/mylist/adapter/MyListRVAdapter.kt
@@ -1,10 +1,14 @@
 package com.lee989898.todayeat.src.mylist.adapter
 
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lee989898.todayeat.R
+import org.w3c.dom.Text
 
 class MyListRVAdapter(val items : ArrayList<String>): RecyclerView.Adapter<MyListRVAdapter.ViewHolder>() {
 
@@ -24,7 +28,13 @@ class MyListRVAdapter(val items : ArrayList<String>): RecyclerView.Adapter<MyLis
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
         fun bindItems(item: String){
+            val image = itemView.findViewById<ImageView>(R.id.my_like_item_iv)
+            val name = itemView.findViewById<TextView>(R.id.my_like_item_tv)
+            val heartOff = itemView.findViewById<ImageView>(R.id.detail_heart_off_iv)
+            val heartOn = itemView.findViewById<ImageView>(R.id.detail_heart_on_iv)
 
+            // 나중에 서버랑 연결
+//            image = item.
         }
 
     }
